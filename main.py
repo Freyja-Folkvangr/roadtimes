887164b01f598133ba4e508c9de3bf6265d81702
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-09-30 08:49:56 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import dijkstra
+
+
+def list_items_to_int(list):
+    return [int('%.0f' % float(item)) for item in list]
+
+
+def create_graph(matrix):
+    i = 0
+    j = 0
+    graph = []
+    while i < len(matrix):
+        while j < len(matrix[i]):
+            # only add the node if there is a route
+            if matrix[i][j] > 0:
+                graph.append((str(i), str(j), matrix[i][j]))
+            j += 1
+        i += 1
+        j = 0
+    # this prints the nodes
+    # print(graph)
+    return dijkstra.Graph(graph)
+
+
+class Trip:
+    def __init__(self, graph, total_time, s, d):
+        self.graph = graph
+        self.total_time = total_time
+        self.s = str(s)
+        self.d = str(d)
+
+    @property
+    def path(self):
+        return self.graph.dijkstra(self.s, self.d)[0]
+
+    @property
+    def total_distance(self):
+        return self.graph.dijkstra(self.s, self.d)[1]
+
+    @property
+    def sub_trips(self):
+        return [[self.path[i], self.path[i + 1]] for i in range(0, len(self.path) - 1)]
+
+    @property
+    def best_time(self):
+        times = []
+        for item in self.sub_trips:
+            times.append(self.get_edge_best_time(item[0], item[1]))
+        return sum(times)
+
+    @property
+    def worst_time(self):
+        return None
+
+    def get_edge_best_time(self, start, end):
+        edge = self.graph.get_edge(start, end)
+        return 60 * edge.cost / 60
+
+    def get_edge_worst_time(self, start, end):
+        return None
+
+    def get_edge_speed(self, start, end):
+        edge = self.graph.get_edge(start, end)
+        return 60 * edge.cost / (self.total_time * (1 - edge.cost / self.total_distance))
+
+
+# Program START
+for file in range(1, 4):
+    try:
+        with open('./data/{}.in'.format(file), 'r') as f:
+            routes = []
+            past_deliveries = []
+            scheduled_queries = []
+            stage = 0
+
+            for line in f.readlines():
+                line = line.replace('\n', '').split(' ')
+
+                if stage == 0 and len(line) == 1:
+                    pass
+                elif stage == 1 and len(line) > 1:
+                    routes.append(list_items_to_int(line))
+
+                elif stage == 2 and len(line) > 1:
+                    past_deliveries.append(list_items_to_int(line))
+
+                elif stage == 3 and len(line) > 1:
+                    scheduled_queries.append(line)
+
+                if len(line) == 1:
+                    stage += 1
+            f.close()
+
+        # calculate minimum time and speed
+        the_graph = create_graph(routes)
+        trips = []
+        for item in past_deliveries:
+            trips.append(Trip(the_graph, item[2], item[0], item[1]))
+
+        # print results
+        print('RESULTS FOR FILE {}'.format(file))
+        for item in scheduled_queries:
+            s = item[0]
+            d = item[1]
+            print(s, d, trips[0].get_edge_best_time(s, d), trips[0].get_edge_worst_time(s, d))
+
+        # print(past_deliveries)
+        # print(the_graph.dijkstra('0', '2'))
+
+    except() as err:
+        print(err)
